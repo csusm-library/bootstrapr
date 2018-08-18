dee836eb36f5048f43f1aa37db7eb12459c8e7c9
Randal Sean Harrison <rharriso@nd.edu>
2018-08-18 01:06:09 -0400

    added all the main builder pages

--- js/main.js
+++ js/main.js
@@ -18,6 +18,30 @@ $(document).ready(function() {
    $('pre').removeClass('prettyprinted');
       prettyPrint();
 
+// Back-to-top button
+
+   // When the user scrolls down 20px from the top of the document, show the button
+   window.onscroll = function() {scrollFunction()};
+
+   function scrollFunction() {
+       if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+           document.getElementById("topper").style.display = "block";
+       } else {
+           document.getElementById("topper").style.display = "none";
+       }
+   }
+
+   // When the user clicks on the button, scroll to the top of the document
+   function topFunction() {
+       document.body.scrollTop = 0; // For Safari
+       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   }
+
+   $("#topper").on("click", function(){
+      $("body").animate({scrollTop: 0}, 400);
+   });
+
+
 });
 // document ready closes
 
