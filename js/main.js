1c059cd37f272773ba4355c8fcd344f0bc55f6f1
Randal Sean Harrison <rharriso@nd.edu>
2020-02-09 16:43:31 -0500

    added a modal confirmation to the reset button

--- js/main.js
+++ js/main.js
@@ -198,6 +198,41 @@ $(document).ready(function() {
   });
 
 
+  $("#dialog-confirm").dialog({
+           autoOpen: false,
+           resizable: false,
+           height: "auto",
+           width: 400,
+           modal: true,
+           buttons: {
+             "Cancel": function() {
+               $(this).dialog("close");
+             },
+             "Reset Form": function() {
+               // Turns the green checked circles to open empty circles
+               window.location.reload(true);
+               //
+               // // Stop propagation of the event
+               // event.stopPropagation();
+               // // Close the dialog
+               // $("#dialog-confirm").dialog("close");
+             }
+           }
+         });
+
+     $("#clear-form").on("click", function() {
+
+           // Open the dialog box onClick
+           $("#dialog-confirm").dialog("open");
+
+           // remove the close button on the modal
+           $(".ui-dialog-titlebar-close").remove();
+           $(".ui-icon-alert").remove();
+           $(".ui-dialog-buttonset button:first-of-type").addClass("btn btn-default");
+           $(".ui-dialog-buttonset button:last-of-type").addClass("btn btn-primary");
+         });
+
+
   // Init the animations package
   AOS.init();
 
